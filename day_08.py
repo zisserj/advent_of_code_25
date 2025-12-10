4477fa07be5bde09f66f58302de3de561d4ef00e
Jules Zisser <59047449+zisserj@users.noreply.github.com>
2025-12-10 10:53:58 +0200

    Added day 8 pt 2

--- day_08.py
+++ day_08.py
@@ -1,4 +1,4 @@
-
+        # print(ind)
 import numpy as np
 
 with open('input_08.txt') as f:
@@ -39,20 +39,23 @@ def calc_distances(boxes):
 
 
 
-def connect_boxes(links, boxes, dists):
+def connect_boxes(boxes, dists, links=10):
     circuits = [] #[[i] for i in range(len(boxes))]
-    for _ in range(links):
+    dists = np.copy(dists)
+    
+    if links == -1:
+        links = 2**30
+
+    itr = 0
+    while itr < links:
         # finds next shortest link
         ind = np.unravel_index(np.argmin(dists, axis=None), dists.shape)
-        if type(ind) != tuple:
-            print(ind)
-        # print(boxes[ind[0]], boxes[ind[1]])
-        # print(ind)
         # check if any of the boxes b are in an existing circuit c
         circuit_idx = -1
         to_delete = -1
         for i, c in enumerate(circuits):
-            if circuit_idx == -1:
+            if circuit_idx == -1: # first end
+                prior = len(c)
                 if ind[0] in c:
                     circuit_idx = i
                     circuits[i].add(ind[1])
@@ -73,17 +76,32 @@ def connect_boxes(links, boxes, dists):
         # change dist between the two as inf
         dists[ind[0], ind[1]] = dists[ind[1], ind[0]] = np.inf
         
-        #print([[i  for i in c] for c in circuits])
+        # print(itr, boxes[ind[0]], boxes[ind[1]])
+        # print(circuits)
+        if len(circuits[0]) >= len(boxes):
+            print(f'stopped at {itr}')
+            return ind
+        
+        itr += 1
     return circuits
 
 def q1(circuits):
     lengths = sorted([len(c) for c in circuits], reverse=True)
     print(lengths[0] * lengths[1] * lengths[2])
-    
+
+def q2(boxes, ind):
+    print(boxes[ind[0]])
+    print(boxes[ind[1]])
+    print(boxes[ind[0], 0] * boxes[ind[1], 0])
+
 lines = content.split()
 boxes = np.array([[int(i) for i in line.split(',')] for line in lines])
 dists = calc_distances(boxes)
 print('finished distances')
-circuits = connect_boxes(1000, boxes, dists)
 
+
+circuits = connect_boxes(boxes, dists, 10)
 q1(circuits)
+
+last_two = connect_boxes(boxes, dists, -1)
+q2(boxes, last_two)
